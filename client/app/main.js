be089b5adbfc94c0da6ccf897a5f876b928b49c8
Glenn Spinner <glenngineer1@gmail.com>
2016-09-26 10:57:15 -0500

    added bootstrap

--- client/app/main.js
+++ client/app/main.js
@@ -2,6 +2,9 @@
 
 angular
   .module('mean101', [])
-  .controller('main', function ($scope) {
-    $scope.title = 'MEAN 101'
+  .controller('main', function ($scope, $http) {
+    $http
+      .get('/api/title')
+      .then(({ data: { title }}) =>
+        $scope.title = (title))
   })
--- server/server.js
+++ server/server.js
@@ -5,7 +5,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-// middlewares
 app.use(express.static('client'))
+
+app.get('/api/title', (req, res) =>
+  res.json({ title: 'MEAN 101 from Node' })
+)
 
 app.listen(port, () => console.log(`Listening on port: ${port}`))
