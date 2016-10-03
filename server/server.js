783f6c32d5711bb7fd22865eeba4609ccf2b08c1
Glenn Spinner <glenngineer1@gmail.com>
2016-10-03 11:21:07 -0500

    using rest and WS(websocket)

--- server/server.js
+++ server/server.js
@@ -55,9 +55,6 @@ io.on('connection', socket => {
   console.log(`Socket connected: ${socket.id}`)
   socket.on('disconnect', () => console.log(`Socket disconnected: ${socket.id}`))
   socket.on('postMessage', createMessage)
-
-
-
 })
 
 function createMessage (reqOrMsg, res, next) {
