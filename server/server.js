49b7945030424d4980651dd76f24a069b079a382
Glenn Spinner <glenngineer1@gmail.com>
2016-10-03 10:20:14 -0500

    Initial websocket implementation

--- server/server.js
+++ server/server.js
@@ -1,17 +1,20 @@
 'use strict'
 
 const { json } = require('body-parser')
+const { Server } = require('http')
 const express = require('express')
 const mongoose = require('mongoose')
+const socketio = require('socket.io')
 
 const app = express()
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/meanchat'
-const port = process.env.PORT || 3000
+const server = Server(app)
+const io = socketio(server)
 
-// middlewares
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/meanchat'
+const PORT = process.env.PORT || 3000
+
 app.use(express.static('client'))
 app.use(json())
-//
 
 app.get('/api/title', (req, res) =>
   res.json({ title: 'MEAN Chat' })
@@ -29,15 +32,31 @@ app.get('/api/messages', (req, res, err) =>
     .catch(err)
 )
 
-app.post('/api/messages', (req, res, err) => {
-  const msg = req.body
+app.post('/api/messages', (req, res, err) =>
   Message
-    .create(msg)
-    .then(msg => res.json(msg))
+    .create(req.body)
+    .then(msg => res.status(201).json(msg))
     .catch(err)
-})
+)
+
+app.use('/api', (req, res) =>
+  res.status(404).send({ code: 404, status: 'Not Found' })
+)
+
+app.use((req, res) =>
+  res.sendFile(process.cwd() + '/client/index.html')
+)
+
+app.use((err, req, res, next) =>
+  res.status(500).send({ code: 500, status: 'Internal Server Error', detail: err.stack })
+)
 
 mongoose.Promise = Promise
 mongoose.connect(MONGODB_URL, () =>
-  app.listen(port, () => console.log(`Listening on port: ${port}`))
+  server.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
 )
+
+io.on('connection', socket => {
+  console.log(`Sockeet connected: ${socket.id}`)
+  socket.on('disconnect', () => console.log(`Socket disconnected: ${socket.id}`))
+})
